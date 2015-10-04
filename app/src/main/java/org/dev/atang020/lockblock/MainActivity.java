fc5528a59e4ba9c54236f0a1f6aeb61e911a83d2
Alex Tang <atang1994@gmail.com>
2015-10-03 19:49:35 -0700

    implemented timer

--- app/src/main/java/org/dev/atang020/lockblock/MainActivity.java
+++ app/src/main/java/org/dev/atang020/lockblock/MainActivity.java
@@ -1,16 +1,126 @@
 package org.dev.atang020.lockblock;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.Format;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+
+    //used to store user's time input in minutes
+    private int timer_Start = 0;
+
+    //used to store the amount of time left
+    private long timeLeft = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*
+        Button b1 = (Button) findViewById(R.id.button);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleAirplaneMode();
+            }
+        });
+*/
+        /*
+        timerTextView = (TextView) findViewById(R.id.timerTextView);
+
+        Button b = (Button) findViewById(R.id.button);
+        b.setText("start");
+        b.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Button b = (Button) v;
+                if (b.getText().equals("stop")) {
+                    timerHandler.removeCallbacks(timerRunnable);
+                    b.setText("start");
+                } else {
+                    startTime = System.currentTimeMillis();
+                    timerHandler.postDelayed(timerRunnable, 0);
+                    b.setText("stop");
+                }
+            }
+        });
+        */
+
+        //clears the user time input
+        final EditText editText = (EditText) findViewById(R.id.time_editText);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.getText().clear();
+            }
+        });
+
+
+        //starts the time
+        Button b1 = (Button) findViewById(R.id.button);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer_Start = Integer.parseInt(editText.getText().toString());
+                timer_Start = 60000*timer_Start;
+                startTime(timer_Start);
+            }
+        });
+
+
+
+    }
+
+
+    //method for countdown
+    public void startTime(int time){
+        final String FORMAT = "%02d:%02d:%02d";
+        final String FORMAT1 = "%s and %s";
+        final TextView text1=(TextView)findViewById(R.id.timerTextView);
+
+        new CountDownTimer(time, 1000) { // adjust the milli seconds here
+
+            public void onTick(long millisUntilFinished) {
+                timeLeft = millisUntilFinished;
+
+                /* FOR STEVEN'S NOTIFICATION FORMAT */
+                text1.setText("Time Remaining: "+String.format(FORMAT1,
+                        Long.toString(TimeUnit.MILLISECONDS.toHours(millisUntilFinished))+" hours",
+                        Long.toString(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(
+                                TimeUnit.MILLISECONDS.toHours(millisUntilFinished)))+" minutes left"));
+                /*text1.setText("seconds remaining:"+String.format(FORMAT,
+                        TimeUnit.MILLISECONDS.toHours(millisUntilFinished),
+                        TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(
+                                TimeUnit.MILLISECONDS.toHours(millisUntilFinished)),
+                        TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(
+                                TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
+                                */
+            }
+
+            public void onFinish() {
+                text1.setText("done!");
+            }
+        }.start();
+
     }
 
     @Override
@@ -33,5 +143,54 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    //prompts airplane mode for user
+    public void toggleAirplaneMode(){
+
+        AlertDialog.Builder db = new AlertDialog.Builder(this);
+        db.setTitle("Sorry!");
+        db.setMessage("We recommend turning on Airplane mode.\n"
+                + "Please enable Airplane mode.\n");
+
+        db.setPositiveButton("Open Settings", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
+            }
+        });
+        db.setNegativeButton("Close app", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        db.show();
+    }
+
+    TextView timerTextView;
+    long startTime = 0;
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            long millis = System.currentTimeMillis() - 100;
+            int seconds = (int) (millis / 1000);
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+
+            timerTextView.setText(String.format("%d:%02d", minutes, seconds));
+
+            timerHandler.postDelayed(this, 500);
+        }
+    };
+
+    public void onPause() {
+        super.onPause();
+        timerHandler.removeCallbacks(timerRunnable);
+        Button b = (Button)findViewById(R.id.button);
+        b.setText("start");
     }
 }
